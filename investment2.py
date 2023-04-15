02dd802ad5ad2f86ed36ad6da3d19507ba2b4732
Lawrence Akpan <delaw34@yahoo.com>
2023-04-16 00:31:20 +0100

    Update investment2.py

--- investment2.py
+++ investment2.py
@@ -15,7 +15,8 @@ def investment():
        account_name = (input('Enter your account name here: '))
        print('Welcome!', account_name, 'to cohort 4 Investment application')
        wallet = int(input("Enter your investment amount here: N "))
-       wallet_balance = wallet * (1 + (profit_percentage / 100))
+       intrest = wallet * profit_percentage / 100
+       wallet_balance = wallet + intrest
        print(account_name,'Your wallet has been credited with the sum of:','N', wallet_balance,"with a 50% interest on your capital")
        withdrawal_amount = int(input("Enter amount you want to withdraw: N"))
        if withdrawal_amount >= wallet_balance :
@@ -23,6 +24,7 @@ def investment():
          
        else: 
            final_wallet_balance = wallet_balance - withdrawal_amount
-           final_balance = final_wallet_balance * (1 - (vat_percentage /100))
+           vat_reduction = final_wallet_balance * vat_percentage /100
+           final_balance = final_wallet_balance - vat_reduction
            print(account_name,"your withdrawal transaction was successful and your acount has been debited with:",'N', withdrawal_amount , "and a 7% service charge has been deducted and ",'your final balance is:' ,'N', final_balance)
 investment()
