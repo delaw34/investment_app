f7a68926a7b94a5fadaa4abd9d473a31c56eee19
Lawrence Akpan <delaw34@yahoo.com>
2023-04-17 22:54:03 +0100

    Update investment2.py

--- investment2.py
+++ investment2.py
@@ -1,30 +1,31 @@
-#investment app
-# user adds name
-# user add amount
-# application returns 50% profits on capital
-# user enter withdraws amount
-# user get a failed transaction if input amount is higher than account balance
-# user gets credited  if input amount is lower than account balance
-# application get 7% service charge from account balance
+#investment application
+#user enters name of the investor
+#user enters the amount to invest
+#application returns 50% profit on capital
+#user enters amount to withdraw
+#user gets a failed transaction if the inputed withdrawal amount is higher than the wallet balance
+#user withdrawal transaction is successful if withdrawal amount is lower than the wallet balance
+#the application deducts a 7% vat from the wallet balance
 
-profit_percentage = 50
-vat_percentage = 7
+
+Profit_percentage = 50
+Vat_percentage = 7
 
 
 def investment():
-       account_name = (input('Enter your account name here: '))
-       print('Welcome!', account_name, 'to cohort 4 Investment application')
+       name_of_investor = (input('Enter your account name here: '))
+       print('Welcome!', name_of_investor, 'to Cohort 4 Investment application')
        wallet = int(input("Enter your investment amount here: N "))
-       intrest = wallet * profit_percentage / 100
-       wallet_balance = wallet + intrest
-       print(account_name,'Your wallet has been credited with the sum of:','N', wallet_balance,"with a 50% interest on your capital")
+       interest_calc = wallet * Profit_percentage / 100
+       wallet_balance = wallet + interest_calc    
+       print(name_of_investor,'Your wallet has been credited with the sum of:','N', wallet_balance,"with a 50% interest on your capital")
        withdrawal_amount = int(input("Enter amount you want to withdraw: N"))
        if withdrawal_amount >= wallet_balance :
-          print(account_name," your transaction failed due to insufficient funds")
+          print(name_of_investor," your transaction failed due to insufficient funds")
          
        else: 
            final_wallet_balance = wallet_balance - withdrawal_amount
-           vat_reduction = final_wallet_balance * vat_percentage /100
-           final_balance = final_wallet_balance - vat_reduction
-           print(account_name,"your withdrawal transaction was successful and your acount has been debited with:",'N', withdrawal_amount , "and a 7% service charge has been deducted and ",'your final balance is:' ,'N', final_balance)
-investment()
+           vat_calc = final_wallet_balance * Vat_percentage /100
+           final_balance = final_wallet_balance - vat_calc   
+           print(name_of_investor,"your withdrawal transaction was successful and your account has been debited with:",'N', withdrawal_amount , "and a 7% service charge", vat_calc, "has been deducted and ", 'your final balance is:' ,'N', final_balance)
+investment() 
